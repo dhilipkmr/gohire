b0612309159224499fd027c42bb2c3238a3e3923
dhilipkumarV <dhilip.velusamy@go-mmt.com>
2019-07-26 20:42:31 +0530

    html skeleton editor

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+const MonacoWebpackPlugin = require(`monaco-editor-webpack-plugin`);
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new MonacoWebpackPlugin()
+    ]
+  });
+};
--- src/pages/editor.js
+++ src/pages/editor.js
@@ -4,7 +4,21 @@ import Wrapper from '../components/Wrapper';
 import HeadingLayout from '../components/HeadingLayout';
 
 class Editor extends React.Component {
+  componentDidMount() {
+    import("monaco-editor").then(monaco => {        // HERE!!
+      monaco.editor.create(document.getElementById('container'), {
+        value: [
+          'function x() {',
+          '\tconsole.log("Hello world!");',
+          '}'
+        ].join('\n'),
+        language: 'javascript'
+      });
+    })
+  }
+
   render() {
+    console.log(this);
     return (
       <Wrapper>
         <HeadingLayout>
@@ -12,7 +26,8 @@ class Editor extends React.Component {
            <div className="contest">
             <div className="fb fq ico30 mt30 mb75">Question</div>
               <div className="ico20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?</div>
-              <textarea/>
+              <div className="textleft" id="container" style={{ width: 800, height: 600, border: "1px solid #ccc", margin: "20px auto" }}></div>
+              <div className=" fr mr80 fss hand challengeBtn expand transAll hoverShadow tdNone white" >Submit</div>
            </div>
           </div>
         </HeadingLayout>
